bbca75e7fbd5f4044c0257bc9c24a93ac50c3573
Vishu Verma <visheshbaund@gmail.com>
2025-02-15 16:50:35 +0530

    Initialize Project prerequisite...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='p-24' >
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
